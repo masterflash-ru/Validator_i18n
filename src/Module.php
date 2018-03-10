e87d9eb14e09ae8f9b300a9174a2436740d49947
Masterflash <sxq@yandex.ru>
2018-03-10 11:57:47 +0300

    f

--- src/Module.php
+++ src/Module.php
@@ -1,25 +1,27 @@
 <?php
 /**
-	модуль перевода в валидаторе, странно зачем разработчики разорвали стандартный i18 и валидаторы
-	весь этот модуль нужен для того что бы по сути к стандартному Translator был привязан интерфейс который полностью повторяет стандартный 
-	из i18n  
- */
+* модуль перевода в валидаторе, подключает модуль с переводами для валидаторовы
+*/
 
 namespace Mf\Validator_i18n;
 use Zend\Validator\Translator\TranslatorInterface;
+use Mf\Validator_i18n\Translator\TranslatorDelegator;
 
 class Module
 {
-    /**
-     */
     public function getConfig()
     {
         return [
-				'service_manager' => [
-					'factories' => [
-					TranslatorInterface::class => Translator\TranslatorServiceFactory::class,
-					],
-				]
+            'service_manager' => [
+                'factories' => [
+                    TranslatorInterface::class => Translator\TranslatorServiceFactory::class,
+                ],
+                'delegators' => [
+                    TranslatorInterface::class => [
+                        TranslatorDelegator::class,
+                    ],
+                ],
+            ],
         ];
     }
 
---
+++ src/Translator/TranslatorDelegator.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ модуль перевода в валидаторе, сам делегаты
+*/
+
+namespace Mf\Validator_i18n\Translator;
+
+
+use Interop\Container\ContainerInterface;
+use Zend\I18n\Translator\Resources;
+use Zend\ServiceManager\DelegatorFactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class TranslatorDelegator implements DelegatorFactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $name, callable $callback, array $options = null)
+    {
+        $translator = $callback();
+
+        $translator->addTranslationFilePattern(
+            'phpArray',
+            Resources::getBasePath(),
+            Resources::getPatternForValidator()
+        );
+        $translator->addTranslationFilePattern(
+            'phpArray',
+            Resources::getBasePath(),
+            Resources::getPatternForCaptcha()
+        );
+
+        return $translator;
+    }
+
+    public function createDelegatorWithName(ServiceLocatorInterface $container,$name, $requestedName, $callback) 
+    {
+        return $this($container, $requestedName, $callback);
+    }
+}
